9834b1d92f5dc31d67597d44d13ef6f4331774f9
rhetr <rhetr@users.noreply.github.com>
2018-03-01 14:21:04 -0500

    update setup

--- setup.sh
+++ setup.sh
@@ -1,16 +1,22 @@
 ## config
+## needs root
 
 # install
 apt install -y Xorg mplayer
-# might not need these
-apt install -y xinit x11-xserver-utils xserver-xorg-legacy
 
-## setup startup
+## might not need these
+# apt install -y xinit x11-xserver-utils xserver-xorg-legacy
+
 echo needs_roots_rights=yes >> /etc/X11/Xwrapper.config
 
-# add controller to xhost
-# DISPLAY=:0 xhost +
+## install videowall to /opt
+cp -t /opt/videowall/ \
+    mplay.py videowall.py load-videowall.sh
+cp 60-videowall.rules /etc/udev/rules.d/
 
+## autostart Xorg
 
-## run
-# prepare slaves
+## USERNAME and USER_HOME should be exported by node/run
+# USERNAME=perry
+# USER_HOME=/home/$USERNAME
+echo '[[ $(tty) = "/dev/tty1" ]] && exec Xorg' >> $USER_HOME/.bashrc
